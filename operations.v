2da722a3039c6909ef302c7c05fdfb2d238ff07b
ravikumar314 <70259868+ravikumar314@users.noreply.github.com>
2020-08-26 13:50:46 +0530

    Create operations.v

---
+++ operations.v
@@ -0,0 +1,7 @@
+`define Inputfile "IMGx.hex" 
+`define Outputfile "output.bmp"
+		
+//`define CONTRAST_OPERATION
+//`define BRIGHTNESS_OPERATION
+//`define INVERT_OPERATION
+`define THRESHOLD_OPERATION 
